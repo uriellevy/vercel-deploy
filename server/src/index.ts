df3ba9e47a9937d0098284af2209fbbf0ae6070d
uriellevy <uriellevy4892@gmail.com>
2025-03-18 16:15:26 +0200

    d

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 app.use(express.json());
 app.use("/api", routes);
 
