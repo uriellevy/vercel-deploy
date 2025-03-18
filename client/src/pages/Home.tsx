337d6eeb83c9379338e01c090cd1c3d90921b395
uriellevy <uriellevy4892@gmail.com>
2025-03-18 15:22:58 +0200

    .env added

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,13 +3,15 @@ import React, { useEffect } from 'react'
 
 const Home = () => {
   const [posts, setPosts] = React.useState([])
+  const apiUrl = import.meta.env.VITE_API_URL;
+  console.log(apiUrl)
   useEffect(() => {
     getPosts();
   },[])
 
   const getPosts = async () => {
     try {
-      const data = await axios.get('http://localhost:5000/api/posts');
+      const data = await axios.get(`${apiUrl}/posts`);
       setPosts(data.data);
     } catch (error) {
       console.log(error)
