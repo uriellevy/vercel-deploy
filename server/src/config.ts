a178bd525f2ee777db07430fc3260fcb8ebb026b
uriellevy <uriellevy4892@gmail.com>
2025-03-20 08:48:22 +0200

    f

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydb";
+const MONGO_URI = process.env.MONGO_ATLAS_URI || "mongodb://localhost:27017/mydb";
 console.log(process.env.MONGO_URI)
 
 const connectDB = async () => {
