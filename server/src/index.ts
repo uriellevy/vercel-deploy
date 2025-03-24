01a27b6e20eff22f98e4c0b9b4980f01a1716edb
uriellevy <uriellevy4892@gmail.com>
2025-03-24 12:54:20 +0200

    s

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ const PORT = process.env.PORT;
 app.use(
   cors({
     origin: ["http://localhost:5174", "https://vercel-deploy-frontend-two.vercel.app"],
+    credentials: true, // Allow cookies to be sent to the frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
   })
